79ff20f8a0cd526f703a7b0d5bfb69ca2ec406a3
Shubham-0-0-7 <shubhamchhatbar1@gmail.com>
2026-03-13 22:30:29 +0530

    some basic knowledge and testing with gdb

--- my.c
+++ my.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 
-struct header{
-  size_t s;
-  header* next;
-}
-
 int main(void){
-    printf("hellow\n");
-    void* start = sbrk(0);
-    void* new_mem = sbrk(1024);
-    
+    char *mem = (char*)sbrk(0);
+    mem = (char*)sbrk(10);
+    *mem = "helloneell";
+
 }
