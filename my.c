98335ecc97ad43845d3debf50621facd28200f63
Shubham-0-0-7 <shubhamchhatbar1@gmail.com>
2026-03-18 23:40:04 +0530

    added code for splitting of blocks

--- my.c
+++ my.c
@@ -23,6 +23,16 @@ header_t* get_free_block(size_t s){
   return NULL;
 }
 
+void my_free(void* ptr){
+  header_t* header;
+  if(!ptr) return;
+  pthread_mutex_lock(&global_malloc_lock);
+  header = (header_t*)ptr - 1;
+  header->is_free = 1;
+  pthread_mutex_unlock(&global_malloc_lock);
+}
+
+
 void* my_alloc(size_t size){
   /*void *block
   block = sbrk(size);
@@ -37,7 +47,18 @@ void* my_alloc(size_t size){
   pthread_mutex_lock(&global_malloc_lock);
 
   header = get_free_block(size);
+
   if(header){
+    if(header->size >= size + sizeof(header_t)){
+      header_t* new_header = (header_t*)((char*)(header+1)+size);
+      new_header->size = header->size - size - sizeof(header_t);
+      new_header->is_free = 1;
+      new_header->next = header->next;
+
+      header->size = size;
+      header->next = new_header;
+      if(header==tail) tail = new_header;
+    }
     header->is_free = 0;
     pthread_mutex_unlock(&global_malloc_lock);
     return (void *)(header+1);
@@ -63,20 +84,45 @@ void* my_alloc(size_t size){
 
 }
 
+void printmemlist(){
+  header_t* curr = head;
+    printf("current memory state\n");
+    int block_num =1;
+    while(curr){
+        printf("block %d: addr = %p \n size = %zu bytes \n is_free = %d \n next = %p\n", 
+            block_num, 
+            (void*)curr, 
+            curr->size, 
+            curr->is_free, 
+            (void*)curr->next
+        );
+        curr = curr->next;
+        block_num++;
+  }
+  printf("cshdabcjhdjdvchj\n");
+}
 
 int main(void){
+    printf("block of size 100 alloc\n");
+    void *ptr1 = my_alloc(100);
+    printmemlist();
+    printf("freeing block of size 100\n");
+    my_free(ptr1);
+    printmemlist();
+
+    printf("smaller block allocate 20\n");
+    void *ptr2 = my_alloc(20);
+    printmemlist();
+/*
     int *a = my_alloc(sizeof(int));
     int *b = my_alloc(sizeof(int));
-
     *a = 10;
     *b = 20;
-
     printf("a = %d\n", *a);
     printf("b = %d\n", *b);
-
     printf("a address = %p\n", a);
     printf("b address = %p\n", b);
-
+*/
     return 0;
 }
 
